a17062fec266d936bf852608d73e8d1771ad4531
Bruce Galpin <bruce.galpin@arqino.com>
2017-02-13 09:07:28 +0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -3,17 +3,26 @@ http = require('http'),
 express = require('express'),
 socketIO = require('socket.io');
 
-
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+var app = express(),
+server = http.createServer(app),
+io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New connection');
+
+  socket.emit('newMessage', {
+    from: 'Bruce',
+    text: 'Heytza',
+    createdAt: new Date().toString()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
