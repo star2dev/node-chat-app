3d4c21ba1ab15809a4db19cc06be4e833219dad0
Bruce Galpin <bruce.galpin@gmail.com>
2019-10-29 12:20:03 -0400

    fix error in code

--- src/utils/users.js
+++ src/utils/users.js
@@ -34,7 +34,7 @@ const addUser = ({ id, username, room }) => {
 
 // Remove user
 const removeUser = (id) => {
-    const index = users.findIndex(() => user.id === id)
+    const index = users.findIndex((user) => user.id === id)
 
     if (index !== -1) {
         return users.splice(index, 1)[0]
