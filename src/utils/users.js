293db1de2326421fdda12389defefc9f750605ff
Bruce Galpin <bruce.galpin@gmail.com>
2019-10-29 11:41:35 -0400

    socket.io chat app

---
+++ src/utils/users.js
@@ -0,0 +1,60 @@
+const users = []
+
+// Add user
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    displayname = username.trim()
+    username = displayname.toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room, displayname }
+    users.push(user)
+    return { user }
+}
+
+// Remove user
+const removeUser = (id) => {
+    const index = users.findIndex(() => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+// Get user
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+// Get users in room
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
